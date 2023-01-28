2790a6bba2f4ccd60da8c1220553ec8fc20ae189
Laplace <vigooktarioa@gmail.com>
2023-01-29 06:28:13 +0700

    creating transaksi controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,40 +1,61 @@
 // load model for user table
-const userModel = require('../models/index').user
+const userModel = require('../models/index').users
 
 // load operation from Sequelize
 const Op = require('sequelize').Op
 
-
+// TODO export sekali aja
 // create function for read all data
 exports.getAllUser = async (request, response) => {
     // call findAll() to get all data
-    let user = await userModel.findAll()
-    return response.json({
-        success: true,
-        data: user,
-        message: 'All user have been loaded'
-    })
+    try {
+        let users = await userModel.findAll();
+        response.json({
+            success: true,
+            data: users,
+            message: 'All users have been loaded'
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.getOneUser = async (request, response) => {
+    try {
+        let users = await userModel.findAll({
+            where: {
+                id_user: request.params.id_user
+            }
+        });
+        response.json({
+            success: true,
+            data: users,
+            message: 'One user has been loaded'
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 // create function for filter
-exports.findUser = async (request, response) => {
+exports.searchUser = async (request, response) => {
     // define keyword to find data
     let keyword = request.body.keyword
 
     // call findAll() within where clause and
     // operation to find data based on keyword
-    let user = await userModel.findAll({
+    let users = await userModel.findAll({
         where: {
             [Op.or]: [
-                { nama_user: { [Op.substring]: keyword }},
+                { username: { [Op.substring]: keyword }},
                 { role: { [Op.substring]: keyword } }
             ]
         }
     })
     return response.json({
         success: true,
-        data: user,
-        message: 'All members have been loaded'
+        data: users,
+        message: 'Searching success'
     })
 }
 
@@ -86,6 +107,7 @@ exports.updateUser = (request, response) => {
         // if update's process success
         return response.json({
             success: true,
+            data:dataUser,
             message: 'Data user has been updated'
         })
     })
